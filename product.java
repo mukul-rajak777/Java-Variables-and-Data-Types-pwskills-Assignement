241bf7171ceddde47b91d90c5cbc5a5458e28b77
mukul Rajak <mukulrajak72@gmail.com>
2023-01-11 12:02:01 +0530

    intital commit

---
+++ product.java
@@ -0,0 +1,10 @@
+public class product
+{
+// Q1. Take 2 integer values in two variables x and y and print their product.    
+	public static void main(String[] args) {
+	    int a=2;
+	    int b=4;
+	    int c=a*b;
+		System.out.println("The product of a and b is : "+ c);
+	}
+}
