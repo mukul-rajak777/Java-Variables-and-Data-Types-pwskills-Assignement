a1d7c102aec9177b538b991bba95958f32758e86
mukul Rajak <mukulrajak72@gmail.com>
2023-01-11 16:05:08 +0530

    adding swap.java

---
+++ swap.java
@@ -0,0 +1,14 @@
+public class swap {
+    public static void main(String[] args) {
+        //Q5 - Write a Java program to swap two numbers with the help of a third variable.
+        int a=2;
+        int b=3;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("the value of a is " +a);
+        System.out.println(" the value of b is "+ b);
+
+    }
+    
+}
