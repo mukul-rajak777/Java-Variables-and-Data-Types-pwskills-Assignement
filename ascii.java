d4dfc7025dd315f78db73df1673e02ca0b44caae
mukul Rajak <mukulrajak72@gmail.com>
2023-01-11 12:11:16 +0530

    adding message in product.java

---
+++ ascii.java
@@ -0,0 +1,11 @@
+public class ascii {
+    
+    public static void main(String[] args)   
+{  
+ //Q2. Print the ASCII value of character ‘U’.
+char ch1 = 'U';  
+int asciivalue1 = ch1;  
+  System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);  
+ 
+}  
+}
