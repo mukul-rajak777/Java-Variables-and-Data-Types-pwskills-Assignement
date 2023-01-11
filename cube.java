4c1bbc0f8a4eef4d6bb925322790a4a902a15dff
mukul Rajak <mukulrajak72@gmail.com>
2023-01-11 12:47:17 +0530

    adding cube.java

---
+++ cube.java
@@ -0,0 +1,12 @@
+public class cube {
+    
+
+    public static void main(String args[])
+    {
+        //Q4 - Write a Java program to calculate the cube of a number.
+    int a=4;
+    int cube=a*a*a;
+    System.out.println("the cube of a ="+cube);
+    }
+    
+}
